7ed17072d40444388b4fee67d6c562dbdd007156
joebuckley06 <joe@qz.com>
2018-02-16 10:35:58 -0500

    Bulletin Overall Additions
    Add avg engaged time to bulletin df and create bulletin plot function

--- Partnership_Bulletins.py
+++ Partnership_Bulletins.py
@@ -11,6 +11,9 @@ class bulletin_analysis:
     """
     Class to retrieve Bulletin data for a certain client for a time period and return charts
     
+    ex: client = "Accenture"
+    ex: bulletin_dict = {'Bulletin_1':['1184288','1181223'], 
+                     'Bulletin_2': ['1180851','1184618']}
     """
     def __init__(self, client, bulletin_urls):
         self.client = client
@@ -100,6 +103,7 @@ class bulletin_analysis:
         df_bulletin['time_on_site_total'] = df_bulletin['time_on_site_total'].astype(float)
         df_bulletin['total_engaged_time'] = df_bulletin['total_engaged_time'].astype(float)
         df_bulletin = df_bulletin.groupby(['Bulletin'],as_index=False)[metric_list].sum()
+        df_bulletin['avg_engaged_time'] = df_bulletin['total_engaged_time'] / df_bulletin['page_views']
         self.df_bulletin = df_bulletin.copy()
         print("(Rows, Columns) : " + str(self.df_SR.shape))
         print("Overall Bulletin Data")
@@ -260,3 +264,160 @@ class bulletin_analysis:
             pass
         
         plt.tight_layout()
+        
+    def plotting_bulletins(self, metric1='page_views',metric2='avg_engaged_time',metric3='social_actions',metric4='facebook_referrals',
+                     m1_benchmark=0, m2_benchmark=0, m3_benchmark=0, m4_benchmark=0, 
+                     font='Adele Sans', ylabel='ylabel', xlabel='xlabel', 
+                     bar_color='red', bench_color='black'):
+        """
+        Plots Partnership Data for Bulletins
+        """
+        # FONTS
+        rcParams['font.family'] = 'sans-serif'
+        rcParams['font.sans-serif'] = [font]
+        rcParams['font.serif'] = [font]
+        title_font = {'size':'24', 'color':'#404347', 'weight':'normal'}
+        font_name = {'fontname':font}
+        
+        # PLOT CREATION
+        fig = plt.figure(num=None, figsize=(8, 6), dpi=120, facecolor='#fcfcfc', edgecolor='k') 
+        ax1 = fig.add_subplot(2, 2, 1)
+        ax2 = fig.add_subplot(2, 2, 2)
+        ax3 = fig.add_subplot(2, 2, 3)
+        ax4 = fig.add_subplot(2, 2, 4)
+        
+        # All X values
+        x_values = self.df_bulletin['Bulletin']
+        xlabel = "Headlines"
+        # All Y values
+        y_1 = self.df_bulletin[metric1]
+        y_2 = self.df_bulletin[metric2]
+        y_3 = self.df_bulletin[metric3]
+        y_4 = self.df_bulletin[metric4]
+        
+        # PLOT #1
+        x_vals = x_values
+        x_labels = [y[:15]+("...") for y in x_values]
+        y_vals = y_1
+        N = len(x_values)
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35       # the width of the bars
+
+
+        
+        rects1 = ax1.bar(ind, y_vals, width, color=bar_color)
+        x = np.linspace(-.5, len(x_values), 50)
+        if m1_benchmark > 0:
+            bench = np.linspace(m1_benchmark, m1_benchmark, 50)
+            line1, = ax1.plot(x, bench, '--', linewidth=2, color=bench_color)
+        else:
+            pass
+
+        # add some text for labels, title and axes ticks
+        ax1.set_ylabel(metric1)
+        ax1.set_xlabel(xlabel,wrap=True)
+        ax1.set_title(metric1, title_font,**font_name)
+        ax1.set_xticks(ind + width / 2)
+        ax1.set_xticklabels(x_labels, rotation=20,va='top')
+
+        if m1_benchmark > 0:
+            ax1.legend((rects1[0],line1), (self.client,'Benchmark'))
+        else:
+            pass
+        
+        # PLOT #2
+        x_vals = x_values
+        x_labels = [y[:15]+("...") for y in x_values]
+        y_vals = y_2
+        N = len(x_values)
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35       # the width of the bars
+
+
+        
+        rects1 = ax2.bar(ind, y_vals, width, color=bar_color)
+        x = np.linspace(-.5, len(x_values), 50)
+        if m2_benchmark > 0:
+            bench = np.linspace(m2_benchmark, m2_benchmark, 50)
+            line1, = ax2.plot(x, bench, '--', linewidth=2, color=bench_color)
+        else:
+            pass
+
+        # add some text for labels, title and axes ticks
+        ax2.set_ylabel(metric2)
+        ax2.set_xlabel(xlabel,wrap=True)
+        ax2.set_title(metric2, title_font,**font_name)
+        ax2.set_xticks(ind + width / 2)
+        ax2.set_xticklabels(x_labels, rotation=20,va='top')
+
+        if m2_benchmark > 0:
+            ax2.legend((rects1[0],line1), (self.client,'Benchmark'))
+        else:
+            pass
+        
+        # PLOT #3
+        x_vals = x_values
+        x_labels = [y[:15]+("...") for y in x_values]
+        y_vals = y_3
+        N = len(x_values)
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35       # the width of the bars
+
+
+        
+        rects1 = ax3.bar(ind, y_vals, width, color=bar_color)
+        x = np.linspace(-.5, len(x_values), 50)
+        if m3_benchmark > 0:
+            bench = np.linspace(m3_benchmark, m3_benchmark, 50)
+            line1, = ax3.plot(x, bench, '--', linewidth=2, color=bench_color)
+        else:
+            pass
+
+        # add some text for labels, title and axes ticks
+        ax3.set_ylabel(metric3)
+        ax3.set_xlabel(xlabel,wrap=True)
+        ax3.set_title(metric3, title_font,**font_name)
+        ax3.set_xticks(ind + width / 2)
+        ax3.set_xticklabels(x_labels, rotation=20,va='top',ha='right')
+
+        if m3_benchmark > 0:
+            ax3.legend((rects1[0],line1), (self.client,'Benchmark'))
+        else:
+            pass
+        
+        # PLOT #4
+        x_vals = x_values
+        x_labels = [y[:15]+("...") for y in x_values]
+        y_vals = y_4
+        N = len(x_values)
+
+        ind = np.arange(N)  # the x locations for the groups
+        width = 0.35       # the width of the bars
+
+
+        
+        rects1 = ax4.bar(ind, y_vals, width, color=bar_color)
+        x = np.linspace(-.5, len(x_values), 50)
+        if m4_benchmark > 0:
+            bench = np.linspace(m4_benchmark, m4_benchmark, 50)
+            line1, = ax4.plot(x, bench, '--', linewidth=2, color=bench_color)
+        else:
+            pass
+
+        # add some text for labels, title and axes ticks
+        ax4.set_ylabel(metric4)
+        ax4.set_xlabel(xlabel,wrap=True)
+        ax4.set_title(metric4, title_font,**font_name)
+        ax4.set_xticks(ind + width / 2)
+        ax4.set_xticklabels(x_labels, rotation=20,va='top')
+
+        if m4_benchmark > 0:
+            ax4.legend((rects1[0],line1), (self.client,'Benchmark'))
+        else:
+            pass
+        
+        plt.tight_layout()
+
